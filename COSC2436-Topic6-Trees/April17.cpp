d1e7d4a1b1861a5f2852b2c600d634205d285d76
Work <Work@MSI>
2024-04-22 14:23:47 -0500

    Where we ended on April 22

--- COSC2436-Topic6-Trees/April17.cpp
+++ COSC2436-Topic6-Trees/April17.cpp
@@ -11,6 +11,11 @@ using namespace MySpace::DataStructs::Trees;
 
 int main()
 {
+    auto inorderTreeRoot = generateInorderTree(); 
+    cout << inorderTreeRoot->nodeValue << endl; 
+
+    inorderTraverse(inorderTreeRoot); 
+
     //TreeNode<int> tree(1); 
     //tree.left = new TreeNode<int>(2); //be careful of "naked new" (a new that doesn't have a delete) 
     //tree.right = new TreeNode<int>(3); 
@@ -20,18 +25,30 @@ int main()
     //cout << tree.left->left << endl; 
 
 
-    auto exampleTree1 = generateExampleTree1(); 
+    //auto rootOfLevelOrderTree = generateLevelOrderTree(); 
+    ////cout << exampleTree1->left->left->right->nodeValue << endl; 
 
-    //cout << exampleTree1->left->left->right->nodeValue << endl; 
+    //cout << std::boolalpha; 
+    ////cout << levelOrderTreeExample->left->left->right->isLeaf() << endl; 
+    //TreeNode<char>* currentNode = rootOfLevelOrderTree; 
 
-    cout << std::boolalpha; 
-    auto nodeOfInterest = exampleTree1->left->left->left; 
 
-    cout << "Is the node containing the character " << nodeOfInterest->nodeValue << endl; 
+    //while (currentNode != nullptr)
+    //{
+    //    cout << currentNode->nodeValue << endl; 
+    //    currentNode = currentNode->left; 
+    //    
+    //}
 
-    cout << "... a leaf node? " << nodeOfInterest->isLeaf() << endl; 
-    cout << "A breadth-first search (traversal) of that tree yields: " << endl; 
-    levelOrderTraverse(exampleTree1);
+
+    //cout << std::boolalpha; 
+    //auto nodeOfInterest = exampleTree1->left->left->left; 
+
+    //cout << "Is the node containing the character " << nodeOfInterest->nodeValue << endl; 
+
+    //cout << "... a leaf node? " << nodeOfInterest->isLeaf() << endl; 
+    //cout << "A breadth-first search (traversal) of that tree yields: " << endl; 
+    //levelOrderTraverse(exampleTree1);
 
 
     //TreeNode<int> tree(1);
